7cb57bd7098d6bbd76cbf4a96b79b1c256d12d8d
bubbles.way <bubbles.way@gmail.com>
2011-05-16 23:33:55 +0200

    Initial implementation, custom support for 'activate'

---
+++ BuilderTest.groovy
@@ -0,0 +1,51 @@
+/*
+Copyright (c) 2011 bubbles.way@gmail.com
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+/*
+Use groovy 1.8.0+
+Classpath to PlantUMLBuilder and plantuml.jar has to be set, e.g.:
+groovy -cp ~/sw/PlantUml/plantuml.jar:src/main/groovy/ BuilderTest.groovy
+*/
+
+import cz.atlas.bubbles.it.PlantUmlBuilder
+import cz.atlas.bubbles.it.PlantUmlBuilderSeqListener
+import net.sourceforge.plantuml.SourceStringReader
+
+def builder = new PlantUmlBuilder()
+builder.addListener(new PlantUmlBuilderSeqListener()) // add extra support for Seq. diagrams
+builder.plantuml {
+    plant("A->B") // this just copies text to the output, this allows any PlantUML expression
+    activate('B') { // customized builder command activate (see  PlantUmlBuilderSeqListener)
+        plant('B->C')
+        activate('C') {
+            plant('C-->B')
+        }
+        plant('A-->B')
+    } // deactivate added automatically
+}
+println builder.getText()
+
+SourceStringReader s = new SourceStringReader(builder.getText())
+FileOutputStream file = new FileOutputStream("./plantUmlBuilderTest.png")
+s.generateImage(file);
+println s.toString()
+file.close()
---
+++ src/main/groovy/cz/atlas/bubbles/it/PlantUmlBuilder.groovy
@@ -0,0 +1,124 @@
+/*
+Copyright (c) 2011 bubbles.way@gmail.com
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+package cz.atlas.bubbles.it
+
+//Builder model node
+private class Node {
+    def name = ''
+    def parent = null; // parent (or null if this is root node)
+    def attributes = [:] //attributes
+    def value = null //value
+    def children = [] //children nodes
+}
+
+//Listener interface  for node or attributes
+enum ListenerResult {
+    NOT_ACCEPTED,
+    PROCESSED,
+    FAILED,
+}
+
+interface PlantBuilderListener {
+    ListenerResult process(final Node node, IndentPrinter out, boolean postProcess)
+}
+
+// Builder class
+class PlantUmlBuilder extends BuilderSupport {
+    private IndentPrinter out
+    private PrintWriter writer
+    private StringWriter stringWriter
+    private Node root = null //root node of the model
+
+    private List listeners = []
+
+    public PlantUmlBuilder() {
+        stringWriter = new StringWriter()
+        writer = new PrintWriter(stringWriter)
+        out = new IndentPrinter(writer)
+    }
+
+    @Override protected void setParent(Object parent, Object child) {
+        ((Node) parent).children.add(child)
+        ((Node) child).parent = parent
+        if (!root) root = parent
+    }
+
+    @Override protected Object createNode(Object name) {
+        return new Node(name: name)
+    }
+
+    @Override protected Object createNode(Object name, Object value) {
+        return new Node(name: name, value: value)
+    }
+
+    @Override protected Object createNode(Object name, Map attributes) {
+        return new Node(name: name, attributes: attributes)
+    }
+
+    @Override protected Object createNode(Object name, Map attributes, Object value) {
+        return new Node(name: name, value: value, attributes: attributes)
+    }
+
+    def addListener(final PlantBuilderListener listener) {
+        listeners += listener
+    }
+
+    def private printNode(node) {
+        boolean nodeProcessedByListener = false
+        for (l in listeners) {
+            ListenerResult res = l.process(node, out, false)
+            nodeProcessedByListener = (res == ListenerResult.PROCESSED)
+        }
+        if (!nodeProcessedByListener && node.name == 'plant') {
+            out.printIndent()
+            out.println(node.value)
+        } else {
+            if (root == node && node.name == 'plantuml' || nodeProcessedByListener) { // allow dummy top element plantuml
+            } else {
+                println "Unsupported node name ${node.name}"
+            }
+        }
+        node.children.each {
+            out.incrementIndent()
+            printNode(it)
+            out.decrementIndent()
+        }
+        if (nodeProcessedByListener) {
+            for (l in listeners) {
+                ListenerResult res = l.process(node, out, true)
+                nodeProcessedByListener = (res == ListenerResult.PROCESSED)
+            }
+        }
+    }
+
+    public String getText() {
+        StringBuffer buffer = stringWriter.getBuffer()
+        buffer.delete(0, buffer.length()) // clear buffer
+        stringWriter.flush()
+        if (root) {
+            printNode(root)
+        }
+        return "@startuml\n" + buffer.toString() + "@enduml"
+    }
+
+}
