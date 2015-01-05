c1733205e4c94d4d8ab5fcc7736a8034d096abde
Michal Novak <it.novakmi@gmail.com>
2015-01-05 22:53:04 +0100

    removed github mvane repo (replaced with jcenter)

--- templates/scripts/basic/plantSeq.groovy
+++ templates/scripts/basic/plantSeq.groovy
@@ -4,7 +4,8 @@
 
 //If you have Internet connection, use groovy Grab to get dependencies (may take some time for the first time to download jars)
 //Run as ordinary groovy script with command 'groovy <scriptName>.groovy' (or as Linux script './<scriptName>.groovy')
-@GrabResolver(name = 'novakmirepo', root = 'https://github.com/novakmi/novakmirepo/raw/master/releases', m2Compatible = true)
+//@GrabResolver(name = 'novakmirepo', root = 'https://github.com/novakmi/novakmirepo/raw/master/releases', m2Compatible = true)
+//@GrabResolver(name = 'novakmirepo', root = 'http://dl.bintray.com/novakmi/maven', m2Compatible = true)
 @Grab(group = 'net.sourceforge.plantuml', module = 'plantuml', version = '7988')  //for newer versions, update numbers
 @Grab(group = 'org.bitbucket.novakmi', module = 'nodebuilder', version = '0.8.0')
 @Grab(group = 'org.bitbucket.novakmi', module = 'plantumlbuilder', version = '0.4.3')
